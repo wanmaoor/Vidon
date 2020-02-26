cf4060481088196e3dbacdfdb2d9d91142060d65
wanmaoor <wanmao1284879375@outlook.com>
2020-02-26 10:58:19 +0800

    添加观察者模式

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,24 @@
-// import x from './x'
-import Observable from '../packages/Observable'
+import Reactive from '../packages/Reactive'
+import { Subject, Observe} from '../packages/Observe'
 import './style.css'
 
 let obj1 = {
   name: 'wanmao'
 }
 
-Observable(obj1)
+Reactive(obj1)
 
 console.log(obj1.name);
 
 obj1.name = 'oliver'
 
+
 console.log(obj1.name);
+let subject = new Subject()
+let observer = new Observe()
+observer.update = function () {
+  console.log('observer update')
+}
+observer.subscribeTo(subject)  //观察者订阅主题
+
+subject.notify()
